22906bb05aff156366c446c2a438971b0a7208c8
Tyler Levine <tyler@tylerlevine.com>
2017-05-11 20:01:40 -0700

    Update font installer and make it OSX compatible

--- install_fonts.sh
+++ install_fonts.sh
@@ -1,10 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SRCDIR=$HOME/.vim/fonts
-TARGETDIR=$HOME/.fonts
 
-cd $TARGETDIR
-find $SRCDIR -name '*.ttf' -exec ln -s {} . \;
-cd -
+link() {
+    SRCDIR="$1"
+    TARGETDIR="$2"
+    [ ! -e "$TARGETDIR" ] && mkdir -p "$TARGETDIR"
 
-fc-cache -vf $TARGETDIR
+    find "$SRCDIR" -name '*.ttf' -exec cp {} "$TARGETDIR" \;
+}
+
+install_linux() {
+    link "$HOME/.vim/fonts" "$HOME/.fonts"
+    fc-cache -vf "$TARGETDIR"
+}
+
+install_darwin() {
+    link "$HOME/.vim/fonts" "$HOME/Library/Fonts"
+}
+
+if [[ $(uname -s) == "Darwin" ]]
+then
+    install_darwin
+else
+    install_linux
+fi
+
